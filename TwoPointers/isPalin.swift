a70666335553591f715994c205b531112b285dd1
den330 <wo131485508@qq.com>
2024-02-03 08:27:56 -0500

    isPalin

---
+++ TwoPointers/isPalin.swift
@@ -0,0 +1,30 @@
+func isPalindrome(_ s: String) -> Bool {
+    if s.count == 0 || s.count == 1 {
+        return true
+    }
+    var strArr = Array(s.lowercased())
+    var endIndex = strArr.count - 1
+    var startIndex = 0
+    while startIndex < endIndex {
+        let leftChar = strArr[startIndex]
+        let rightChar = strArr[endIndex]
+        var needSkip = false;
+        if !leftChar.isLetter && !leftChar.isNumber {
+            startIndex += 1
+            needSkip = true
+        }
+        if !rightChar.isLetter && !rightChar.isNumber {
+            endIndex -= 1
+            needSkip = true
+        }
+        if needSkip {
+            continue
+        }
+        if leftChar != rightChar {
+            return false
+        }
+        startIndex += 1
+        endIndex -= 1
+    }
+    return true
+}
