8c415f2fe2ee3ad26d572a09447da908aca412dd
Yaxin <yaxinyuan0910@gmail.com>
2024-02-05 12:50:49 -0500

    isPalin

--- TwoPointers/isPalin.swift
+++ TwoPointers/isPalin.swift
@@ -33,5 +33,23 @@ func isPalindrome2(_ s: String) -> Bool {
     if s.count == 0 || s.count == 1 {
         return true
     }
-    
+    var startIndex = s.startIndex
+    var endIndex = s.index(before: s.endIndex)
+    while startIndex < endIndex {
+        while startIndex < endIndex && !s[startIndex].isLetter && !s[startIndex].isNumber {
+            startIndex = s.index(after: startIndex)
+        }
+        while startIndex < endIndex && !s[endIndex].isLetter && !s[endIndex].isNumber {
+            endIndex = s.index(before: endIndex)
+        }
+        if startIndex >= endIndex {
+            return true
+        }
+        if s[startIndex].lowercased() != s[endIndex].lowercased() {
+            return false
+        }
+        startIndex = s.index(after: startIndex)
+        endIndex = s.index(before: endIndex)
+    }
+    return true
 }
