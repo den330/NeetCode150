ad89c733201fa1d88bf89618951fe5fd0c5e871c
Yaxin <yaxinyuan0910@gmail.com>
2024-02-23 19:28:39 -0500

    quicksort2

--- Sort/QuickSort.swift
+++ Sort/QuickSort.swift
@@ -11,3 +11,18 @@ func QuickSort(_ arr: [Int]) -> [Int] {
     let equalArr = arr.filter({$0 == pivot})
     return QuickSort(lowerArr) + equalArr + QuickSort(higherArr)
 }
+
+func QuickSort2(_ arr: inout [Int], _ low: Int, _ high: Int) {
+    if low >= high { return }
+    let pivot = arr[high]
+    var lowSlot = low
+    for i in low..<high {
+        if arr[i] < pivot {
+            arr.swapAt(i, lowSlot)
+            lowSlot += 1
+        }
+    }
+    arr.swapAt(high, lowSlot)
+    QuickSort(&arr,  low, lowSlot - 1)
+    QuickSort(&arr, lowSlot + 1,  high)
+}
